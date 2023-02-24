5adcb70914a3c926cea2eafbfb44ff80d0d94121
sbx_user1051 <sbx_user1051@169.254.83.185>
2023-02-24 04:34:05 +0000

    Update requested for Proton - environment/sample-vpc-environment-template

--- sample-vpc-environment-template/outputs.tf
+++ sample-vpc-environment-template/outputs.tf
@@ -1,25 +1,15 @@
 /*
-This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
-
-To manage this resource, see AWS Proton Resource: arn:aws:proton:us-east-2:338277334018:environment/sample-vpc-environment-template
-
-If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+This file is no longer managed by AWS Proton. The associated resource has been deleted in Proton.
 */
 
 output "vpc_arn" {
   value = module.vpc.vpc_arn
 }
 
-#output "subnet_id" {
-#  value = one(module.vpc.private_subnets) # there is a known issue with terraform lists as outputs for proton
-#}
+output "subnet_id" {
+  value = one(module.vpc.private_subnets) # there is a known issue with terraform lists as outputs for proton
+}
 
 output "security_group_id" {
   value = module.vpc.default_security_group_id
-}
-output "private_subnet_id" {
-  value          = [for az, subnet in aws_subnet.private: subnet.id]
-}
-output "public_subnet_id" {
-  value          = [for az, subnet in aws_subnet.public: subnet.id]
 }
