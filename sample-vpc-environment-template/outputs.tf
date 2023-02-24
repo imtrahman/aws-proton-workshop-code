06b6935a17415884c92f34d5b3c702c58684f4ee
EC2 Default User <ec2-user@ip-172-31-28-101.us-east-2.compute.internal>
2023-02-24 01:21:25 +0000

    Added new terraform code

--- sample-vpc-environment-template/outputs.tf
+++ sample-vpc-environment-template/outputs.tf
@@ -10,10 +10,16 @@ output "vpc_arn" {
   value = module.vpc.vpc_arn
 }
 
-output "subnet_id" {
-  value = one(module.vpc.private_subnets) # there is a known issue with terraform lists as outputs for proton
-}
+#output "subnet_id" {
+#  value = one(module.vpc.private_subnets) # there is a known issue with terraform lists as outputs for proton
+#}
 
 output "security_group_id" {
   value = module.vpc.default_security_group_id
+}
+output "private_subnet_id" {
+  value          = [for az, subnet in aws_subnet.private: subnet.id]
+}
+output "public_subnet_id" {
+  value          = [for az, subnet in aws_subnet.public: subnet.id]
 }
